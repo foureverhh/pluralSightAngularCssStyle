b5ca6e4d01a83ddd8b420340ee84373fdd15fedc
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-05-27 18:53:07 +0200

    ViewEncapsulation to modify whether to show shadowDom

--- src/app/app-nav.component.ts
+++ src/app/app-nav.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'saa-app-nav',
+    encapsulation: ViewEncapsulation.ShadowDom,
     template: `
         <div>
             <ul>
